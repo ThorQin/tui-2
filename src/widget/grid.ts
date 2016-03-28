599e4cb889dfb0989c96732cda4adb9ea1651d06
Thor Qin <thor.qin@outlook.com>
2016-03-28 20:18:43 +0800

    div grid ...

--- src/widget/grid.ts
+++ src/widget/grid.ts
@@ -5,6 +5,15 @@ module tui.widget {
 
 	export class Grid extends Widget {
 		
+		protected init(): void {
+			this._.innerHTML = "<div class='tui-grid-head'></div><div class='tui-content'></div>";
+			var head = this._components["head"] = $(this._).children(".tui-grid-head")[0];
+			var content = this._components["content"] = $(this._).children(".tui-content")[0];
+			$(this._).scroll((e) => {
+				head.style.top = this._.scrollTop + "px";
+			});
+		}
+		
 		render(): void {
 			
 		}
